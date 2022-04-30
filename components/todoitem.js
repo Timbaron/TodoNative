838c6b699c21b5b2f97d9b5b221ce494dc6669ae
timbaron <timothy.akiode@student.aul.edu.ng>
2022-04-30 11:21:20 +0100

    Adding icons to the item list

--- components/todoitem.js
+++ components/todoitem.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 export default function TodoItem({item, pressHandler})
 { 
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>
+            <View style={styles.item}>
+                <MaterialIcons name="delete" size={24} color="black" />
+            <Text style={styles.itemText}>
                 {item.text}
             </Text>
+            </View>
         </TouchableOpacity>
     )
 }
@@ -21,5 +25,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderStyle: 'dashed',
         borderRadius:10,
+        flexDirection: 'row',
+    },
+    itemText: {
+        marginLeft: 10,
     }
 })
