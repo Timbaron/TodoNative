601f40038a6f44f9e443789da265161142f4d007
timbaron <timothy.akiode@student.aul.edu.ng>
2022-04-30 08:01:48 +0100

    Create todo list header and display few items

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,  {useState} from 'react';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Buy Coffee', key: '1' },
+    {text: 'Create an app', key:'2'},
+    { text: 'Play on the switch', key: '3' }
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <View style={styles.content} >
+        {/*Form*/}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding:40,
+  },
+  list: {
+    marginTop:20
+  }
 });
